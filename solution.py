8eabe03bcfea990f403e7978cd43334030acc195
Kleyal <90624521+Kleyal@users.noreply.github.com>
2023-04-14 23:58:53 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
-
+from hyppo.ksample import KSample
 
 chat_id = 405993924 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    significance_level = 0.09
+    pvalue = KSample(indep_test='Dcorr').test(x, y).pvalue
+    if pvalue < significance_level:
+        return True
+    return False
