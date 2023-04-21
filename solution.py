3336663605c459b1044dbdc6d672225bf1a2dcc8
Kleyal <90624521+Kleyal@users.noreply.github.com>
2023-04-21 23:03:35 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,10 @@
 import pandas as pd
 import numpy as np
-from hyppo.ksample import KSample
+from hyppo.ksample import MMD
 
 chat_id = 405993924 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     significance_level = 0.09
-    pvalue = KSample(indep_test='Dcorr').test(x, y).pvalue
-    if pvalue < significance_level:
-        return True
-    else:
-        return False
+    pvalue = MMD(compute_kernel='rbf', gamma=1.0).test(x, y).pvalue
+    return pvalue < significance_level
