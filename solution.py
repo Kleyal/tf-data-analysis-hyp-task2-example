a389832658b71a9cd663501662466fff4f70687f
Kleyal <90624521+Kleyal@users.noreply.github.com>
2023-04-21 23:47:29 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,7 +4,6 @@ from hyppo.ksample import MMD
 
 chat_id = 405993924 # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array, y: np.array) -> bool:
-    significance_level = 0.09
-    pvalue = MMD(compute_kernel='rbf', gamma=1.0).test(x, y).pvalue
-    return pvalue < significance_level
+def solution(x, y):
+    res = MMD(compute_kernel='rbf', gamma=1.0).test(x, y)
+    return res[1] < 0.09
